0325ae62bfb138a0c7337fe7bf78be8ca6568d3d
Andrei Tudor <andrei.tudor27@gmail.com>
2019-04-10 15:28:39 +0200

    Store MQTT

--- mqtt/store.js
+++ mqtt/store.js
@@ -7,16 +7,18 @@ class StoreMqttHandler extends MqttHandler {
 			let arr = topic.split('/')
 			console.log(arr)
 			console.log(message.toString())
-			this.storeData(message.toString(), arr[arr.length - 2])
+			this.storeData(message.toString(), {deviceName: arr[arr.length - 2],regName: arr[arr.length-4]})
 		})
 	}
-	storeData(data, deviceID) {
+	storeData(data, {deviceName, regName}) {
 		try {
 
 			let pData = JSON.parse(data)
-			let query = "INSERT INTO `Device_data`(data,device_name) VALUES ('" + JSON.stringify(pData.data) + '\',\'' + deviceID + "')"
+			let query = "INSERT INTO `Device_data`(data,device_name,reg_name) VALUES ('" + JSON.stringify(pData.data) + '\',\'' + deviceName + '\',\'' + regName + "')"
 			console.log(query)
-			mysqlConn.query(query)
+			mysqlConn.query(query, (err, result) => {
+				if(err) {console.log(err)}
+			})
 		}
 		catch (e) {
 			console.log("ERROR:", e.message)
