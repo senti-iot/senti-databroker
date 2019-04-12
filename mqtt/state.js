4dc357573bdfc6f62228c1efcdaa19fa4de78079
Andrei Tudor <andrei.tudor27@gmail.com>
2019-04-12 16:43:48 +0200

    New stuff

--- mqtt/state.js
+++ mqtt/state.js
@@ -1,21 +1,27 @@
 var MqttHandler = require('./mqtt_handler.js')
 var mysqlConn = require('../mysql/mysql_handler')
 
-class StoreMqttHandler extends MqttHandler {
+class StateMqttHandler extends MqttHandler {
 	init() {
 		this.topic = 'v1/+/location/+/registries/+/devices/+/state/+'
 		this.mqttClient.on('message', (topic, message) => {
 			let arr = topic.split('/')
 			console.log(arr)
 			console.log(message.toString())
-
 			this.storeData(message.toString(), { deviceName: arr[7], regName: arr[5], customerID: arr[1], state: arr[arr.length - 1] })
 		})
 	}
 	storeData(data, { deviceName, regName, customerID, state }) {
 		try {
 			let pData = JSON.parse(data)
-			let query = "INSERT INTO Device_state(`data`, device_id, customer_id, reg_name, state) VALUES ('" + JSON.stringify(pData.data) + '\',\'' + deviceName + '\',\'' + customerID + '\',\'' + regName+ '\',\'' + state + "')"
+			let query = `INSERT INTO Device_state
+			(data, topic, created, device_id, state)
+			SELECT '${JSON.stringify(pData)}', '', NOW(),Device.id as device_id, '${state}' from Registry
+			INNER JOIN Device ON Registry.id = Device.reg_id
+			INNER JOIN Customer ON Customer.id = Registry.customer_id
+			where uuid='${customerID}' AND Device.name='${deviceName}' AND Registry.name='${regName}'
+			`
+			// VALUES ('" + JSON.stringify(pData.data) + '\',\'' + deviceName + '\',\'' + customerID + '\',\'' + regName + "')`
 			console.log(query)
 			mysqlConn.query(query, (err, result) => {
 				if (err) { console.log(err) }
@@ -29,4 +35,4 @@ class StoreMqttHandler extends MqttHandler {
 
 }
 
-module.exports = StoreMqttHandler
+module.exports = StateMqttHandler
--- mqtt/store.js
+++ mqtt/store.js
@@ -8,21 +8,19 @@ class StoreMqttHandler extends MqttHandler {
 			let arr = topic.split('/')
 			console.log(arr)
 			console.log(message.toString())
-			this.storeData(message.toString(), { deviceName: arr[arr.length - 2], regName: arr[arr.length - 4], customerID: arr[1] })
+			this.storeData(message.toString(), { deviceName: arr[7], regName: arr[5], customerID: arr[1] })
 		})
 	}
 	storeData(data, { deviceName, regName, customerID }) {
 		try {
-			/* INSERT INTO Device_data
-			(data, topic, created, device_id)
-			SELECT '{"test":"hello"}', '', NOW(),Device.id as device_id from `Registry`
-			INNER JOIN `Device` ON `Registry`.`id` = `Device`.`reg_id` where customer_id=1 */
-			
 			let pData = JSON.parse(data)
 			let query = `INSERT INTO Device_data
 			(data, topic, created, device_id)
-			SELECT '${JSON.stringify(pData.data)}', '', NOW(),Device.id as device_id from Registry
-			INNER JOIN Device ON Registry.id = Device.reg_id where customer_id=1`
+			SELECT '${pData}', '', NOW(),Device.id as device_id from Registry
+			INNER JOIN Device ON Registry.id = Device.reg_id
+			INNER JOIN Customer ON Customer.id = Registry.customer_id
+			where uuid='${customerID}' AND Device.name='${deviceName}' AND Registry.name='${regName}'
+			`
 			// VALUES ('" + JSON.stringify(pData.data) + '\',\'' + deviceName + '\',\'' + customerID + '\',\'' + regName + "')`
 			console.log(query)
 			mysqlConn.query(query, (err, result) => {
