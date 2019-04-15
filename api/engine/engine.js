7bd733d96a9498d0290561dfbd7823ef13fcd2ea
Andrei Tudor <andrei.tudor27@gmail.com>
2019-04-15 15:33:37 +0200

    send data to rules engine

---
+++ api/engine/engine.js
@@ -0,0 +1,11 @@
+const createAPI = require('apisauce').create
+const dotenv = require('dotenv').load()
+
+
+
+let engineAPI = createAPI({
+	baseURL: 'http://127.0.0.1:3009/',
+	headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }
+})
+
+module.exports = engineAPI
--- mqtt/store.js
+++ mqtt/store.js
@@ -1,19 +1,25 @@
 var MqttHandler = require('./mqtt_handler.js')
 var mysqlConn = require('../mysql/mysql_handler')
-
+const engineAPI = require('../api/engine/engine')
 class StoreMqttHandler extends MqttHandler {
 	init() {
 		this.topic = 'v1/+/location/+/registries/+/devices/+/publish'
 		this.mqttClient.on('message', (topic, message) => {
 			let arr = topic.split('/')
-			console.log(arr)
-			console.log(message.toString())
+			// console.log(arr)
+			// console.log(message.toString())
 			this.storeData(message.toString(), { deviceName: arr[7], regName: arr[5], customerID: arr[1] })
 		})
 	}
-	storeData(data, { deviceName, regName, customerID }) {
+	async storeData(data, { deviceName, regName, customerID }) {
 		try {
+
 			let pData = JSON.parse(data)
+			let deviceQ = `SELECT Device.id, Device.name, Device.type_id, Device.reg_id, Device.normalize from Device
+			INNER JOIN Registry ON Registry.id = Device.reg_id
+			INNER JOIN Customer on Customer.id = Registry.customer_id
+			where uuid='${customerID}' AND Device.name='${deviceName}' AND Registry.name='${regName}';
+			`
 			let query = `INSERT INTO Device_data
 			(data, topic, created, device_id)
 			SELECT '${JSON.stringify(pData)}', '', NOW(),Device.id as device_id from Registry
@@ -21,11 +27,25 @@ class StoreMqttHandler extends MqttHandler {
 			INNER JOIN Customer ON Customer.id = Registry.customer_id
 			where uuid='${customerID}' AND Device.name='${deviceName}' AND Registry.name='${regName}'
 			`
-			// VALUES ('" + JSON.stringify(pData.data) + '\',\'' + deviceName + '\',\'' + customerID + '\',\'' + regName + "')`
-			console.log(query)
-			mysqlConn.query(query, (err, result) => {
-				if (err) { console.log(err) }
+			let lastId = null
+			await mysqlConn.query(query).then(([res, fi])=> {
+				lastId = res.insertId;
 			})
+			let [device, fields] = await mysqlConn.query(deviceQ)
+			if (device[0].normalize === 1) {
+				let normalized = await engineAPI.post('/', { ...JSON.parse(data), flag: device[0].normalize }).then(rs => { console.log(rs.status); return rs.data })
+				// console.log(normalized)
+				let normalizedQ = `INSERT INTO Device_data_clean
+				(data, created, device_id, device_data_id)
+				SELECT '${normalized}', NOW(),Device.id as device_id, ${lastId} from Registry
+				INNER JOIN Device ON Registry.id = Device.reg_id
+				INNER JOIN Customer ON Customer.id = Registry.customer_id
+				where uuid='${customerID}' AND Device.name='${deviceName}' AND Registry.name='${regName}'
+				`
+				await mysqlConn.query(normalizedQ).then().catch(e=> {
+					console.log(e)
+				})
+			}
 		}
 		catch (e) {
 			console.log("ERROR:", e.message)
