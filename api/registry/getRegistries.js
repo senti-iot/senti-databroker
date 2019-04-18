78fe9b7eb1114419a74fe9dd525166e821000f0a
Andrei Tudor <andrei.tudor27@gmail.com>
2019-04-18 16:26:41 +0200

    GetRegistries v2

--- api/registry/getRegistries.js
+++ api/registry/getRegistries.js
@@ -8,23 +8,14 @@ router.get('/:version/:customerID/registries', async (req, res, next) => {
 	let apiVersion = req.params.version
 	let authToken = req.headers.auth
 	let customerID = req.params.customerID
-	console.log('bing')
 	if (verifyAPIVersion(apiVersion)) {
 		if (authenticate(authToken)) {
 			let query = `SELECT * from Registry where customer_id=${customerID}`
-			let[registries, fields] = await mysqlConn.query(query).then(rs => {
-					// console.log(rs)
+			await mysqlConn.query(query).then(rs => {
 					res.status(200).json(rs[0])
-					// res.status(200).json(rs)
 				}).catch(err => {
 					if(err) {res.status(500).json(err)}
 			})
-			
-		
-			// res.json('API/sigfox POST Access Authenticated!')
-			// console.log('API/sigfox POST Access Authenticated!')
-			//Send the data to DataBroker
-			// dataBrokerChannel.sendMessage(`${JSON.stringify(data)}`)
 		} else {
 			res.status(403).json('Unauthorized Access! 403')
 			console.log('Unauthorized Access!')
