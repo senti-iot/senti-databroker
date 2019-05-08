d4fb2cf1ac3a1e01eb30f45d43ea63935b0a4c80
Andrei Tudor <andrei.tudor27@gmail.com>
2019-05-08 14:49:01 +0200

    Test old data format

--- api/deviceData/getDeviceData.js
+++ api/deviceData/getDeviceData.js
@@ -3,7 +3,38 @@ const router = express.Router()
 const verifyAPIVersion = require('senti-apicore').verifyapiversion
 const { authenticate } = require('senti-apicore')
 var mysqlConn = require('../../mysql/mysql_handler')
+const moment = require('moment')
 
+router.get('/v2/devicedata-clean/:deviceID/:from/:to', async (req, res, next) => { 
+	let apiVersion = 'v1'
+	let authToken = req.headers.auth
+	let deviceID = req.params.deviceID
+	let from = req.params.from
+	let to = req.params.to
+	if (verifyAPIVersion(apiVersion)) {
+		if (authenticate(authToken)) {
+			let query = `SELECT id, \`data\`, created, device_id
+			FROM Device_data_clean	
+			WHERE device_id=${deviceID} AND \`data\` NOT LIKE '%null%' AND created >= '${from}' AND created <= '${to}'`
+			await mysqlConn.query(query).then(rs => {
+					let rawData = rs[0]
+					let cleanData = {}
+					rawData.forEach(r=> {
+						cleanData[moment(r.created).format('YYYY-MM-DD HH:mm')] = r.data.value
+					})
+					res.status(200).json(cleanData)
+				}).catch(err => {
+					if(err) {res.status(500).json({err, query})}
+			})
+		} else {
+			res.status(403).json('Unauthorized Access! 403')
+			console.log('Unauthorized Access!')
+		}
+	} else {
+		console.log(`API/sigfox version: ${apiVersion} not supported`)
+		res.send(`API/sigfox version: ${apiVersion} not supported`)
+	}
+})
 router.get('/:version/devicedata-clean/:deviceID/:from/:to', async (req, res, next) => { 
 	let apiVersion = req.params.version
 	let authToken = req.headers.auth
