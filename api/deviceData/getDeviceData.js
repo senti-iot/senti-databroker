a2b3af311122f2470c5cfd1f78df13e9edc3d9ee
Andrei <andrei.tudor27@gmail.com>
2019-05-13 01:05:19 +0200

    dataType

--- api/deviceData/getDeviceData.js
+++ api/deviceData/getDeviceData.js
@@ -5,12 +5,13 @@ const { authenticate } = require('senti-apicore')
 var mysqlConn = require('../../mysql/mysql_handler')
 const moment = require('moment')
 
-router.get('/v2/devicedata-clean/:deviceID/:from/:to', async (req, res, next) => { 
-	let apiVersion = 'v1'
+router.get('/:version/devicedata-clean/:deviceID/:from/:to/:type', async (req, res, next) => { 
+	let apiVersion = req.params.version
 	let authToken = req.headers.auth
 	let deviceID = req.params.deviceID
 	let from = req.params.from
 	let to = req.params.to
+	let type = req.params.type
 	if (verifyAPIVersion(apiVersion)) {
 		if (authenticate(authToken)) {
 			let query = `SELECT id, \`data\`, created, device_id
@@ -20,7 +21,7 @@ router.get('/v2/devicedata-clean/:deviceID/:from/:to', async (req, res, next) =>
 					let rawData = rs[0]
 					let cleanData = {}
 					rawData.forEach(r=> {
-						cleanData[moment(r.created).format('YYYY-MM-DD HH:mm')] = r.data.value
+						cleanData[moment(r.created).format('YYYY-MM-DD HH:mm')] = r.data[type]
 					})
 					res.status(200).json(cleanData)
 				}).catch(err => {
@@ -35,32 +36,64 @@ router.get('/v2/devicedata-clean/:deviceID/:from/:to', async (req, res, next) =>
 		res.send(`API/sigfox version: ${apiVersion} not supported`)
 	}
 })
-router.get('/:version/devicedata-clean/:deviceID/:from/:to', async (req, res, next) => { 
-	let apiVersion = req.params.version
-	let authToken = req.headers.auth
-	let customerID = req.params.customerID
-	let deviceID = req.params.deviceID
-	let from = req.params.from
-	let to = req.params.to
-	if (verifyAPIVersion(apiVersion)) {
-		if (authenticate(authToken)) {
-			let query = `SELECT id, \`data\`, created, device_id
-			FROM Device_data_clean	
-			WHERE device_id=${deviceID} AND \`data\` NOT LIKE '%null%' AND created >= '${from}' AND created <= '${to}'`
-			await mysqlConn.query(query).then(rs => {
-				res.status(200).json(rs[0])
-				}).catch(err => {
-					if(err) {res.status(500).json({err, query})}
-			})
-		} else {
-			res.status(403).json('Unauthorized Access! 403')
-			console.log('Unauthorized Access!')
-		}
-	} else {
-		console.log(`API/sigfox version: ${apiVersion} not supported`)
-		res.send(`API/sigfox version: ${apiVersion} not supported`)
-	}
-})
+
+// router.get('/v2/devicedata-clean/:deviceID/:from/:to', async (req, res, next) => { 
+// 	let apiVersion = 'v1'
+// 	let authToken = req.headers.auth
+// 	let deviceID = req.params.deviceID
+// 	let from = req.params.from
+// 	let to = req.params.to
+// 	if (verifyAPIVersion(apiVersion)) {
+// 		if (authenticate(authToken)) {
+// 			let query = `SELECT id, \`data\`, created, device_id
+// 			FROM Device_data_clean	
+// 			WHERE device_id=${deviceID} AND \`data\` NOT LIKE '%null%' AND created >= '${from}' AND created <= '${to}'`
+// 			await mysqlConn.query(query).then(rs => {
+// 					let rawData = rs[0]
+// 					let cleanData = {}
+// 					rawData.forEach(r=> {
+// 						cleanData[moment(r.created).format('YYYY-MM-DD HH:mm')] = r.data.value
+// 					})
+// 					res.status(200).json(cleanData)
+// 				}).catch(err => {
+// 					if(err) {res.status(500).json({err, query})}
+// 			})
+// 		} else {
+// 			res.status(403).json('Unauthorized Access! 403')
+// 			console.log('Unauthorized Access!')
+// 		}
+// 	} else {
+// 		console.log(`API/sigfox version: ${apiVersion} not supported`)
+// 		res.send(`API/sigfox version: ${apiVersion} not supported`)
+// 	}
+// })
+// router.get('/:version/devicedata-clean/:deviceID/:from/:to', async (req, res, next) => { 
+// 	let apiVersion = req.params.version
+// 	let authToken = req.headers.auth
+// 	let customerID = req.params.customerID
+// 	let deviceID = req.params.deviceID
+// 	let from = req.params.from
+// 	let to = req.params.to
+// 	if (verifyAPIVersion(apiVersion)) {
+// 		if (authenticate(authToken)) {
+// 			let query = `SELECT id, \`data\`, created, device_id
+// 			FROM Device_data_clean	
+// 			WHERE device_id=${deviceID} AND \`data\` NOT LIKE '%null%' AND created >= '${from}' AND created <= '${to}'`
+// 			console.log(query)
+// 			await mysqlConn.query(query).then(rs => {
+// 				res.status(200).json(rs[0])
+// 				}).catch(err => {
+// 					if(err) {res.status(500).json({err, query})}
+// 			})
+// 		} else {
+// 			res.status(403).json('Unauthorized Access! 403')
+// 			console.log('Unauthorized Access!')
+// 		}
+// 	} else {
+// 		console.log(`API/sigfox version: ${apiVersion} not supported`)
+// 		res.send(`API/sigfox version: ${apiVersion} not supported`)
+// 	}
+// })
 router.get('/:version/devicedata/:deviceID', async (req, res, next) => {
 	let apiVersion = req.params.version
 	let authToken = req.headers.auth
