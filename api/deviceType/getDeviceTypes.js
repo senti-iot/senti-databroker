353afd0448b00cd9c2b2d80de382afaa3120e446
Andrei Tudor <andrei.tudor27@gmail.com>
2019-05-20 15:20:19 +0200

    DeviceTypes

--- api/deviceType/getDeviceTypes.js
+++ api/deviceType/getDeviceTypes.js
@@ -10,8 +10,32 @@ router.get('/:version/:customerID/deviceTypes', async (req, res, next) => {
 	let customerID = req.params.customerID
 	if (verifyAPIVersion(apiVersion)) {
 		if (authenticate(authToken)) {
-			let query = `SELECT * from Device_type where customer_id=? and deleted=0`
+			let query = `SELECT * from Device_type t
+			INNER JOIN Customer c on c.id = t.customer_id
+			where c.ODEUM_org_id = ?`
 			await mysqlConn.query(query, [customerID]).then(rs => {
+					res.status(200).json(rs[0])
+				}).catch(err => {
+					if(err) {res.status(500).json(err)}
+			})
+		} else {
+			res.status(403).json('Unauthorized Access! 403')
+			console.log('Unauthorized Access!')
+		}
+	} else {
+		console.log(`API/sigfox version: ${apiVersion} not supported`)
+		res.send(`API/sigfox version: ${apiVersion} not supported`)
+	}
+})
+router.get('/:version/deviceTypes', async (req, res, next) => {
+	let apiVersion = req.params.version
+	let authToken = req.headers.auth
+	let customerID = req.params.customerID
+	if (verifyAPIVersion(apiVersion)) {
+		if (authenticate(authToken)) {
+			let query = `SELECT * from Device_type t
+			INNER JOIN Customer c on c.id = t.customer_id`
+			await mysqlConn.query(query).then(rs => {
 					res.status(200).json(rs[0])
 				}).catch(err => {
 					if(err) {res.status(500).json(err)}
