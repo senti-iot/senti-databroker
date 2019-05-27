ab24d11e1f1e8bd8b5fcd28ec4719f90ce94d196
Andrei Tudor <andrei.tudor27@gmail.com>
2019-05-27 14:46:26 +0200

    get customer name on DeviceTypes

--- api/deviceType/getDeviceTypes.js
+++ api/deviceType/getDeviceTypes.js
@@ -34,7 +34,8 @@ router.get('/:version/devicetypes', async (req, res, next) => {
 	let customerID = req.params.customerID
 	if (verifyAPIVersion(apiVersion)) {
 		if (authenticate(authToken)) {
-			let query = `SELECT * from Device_type`
+			let query = `SELECT t.*, c.name as customer_name from Device_type t
+			INNER JOIN Customer c on c_id = t.customer_id`
 			await mysqlConn.query(query).then(rs => {
 					res.status(200).json(rs[0])
 				}).catch(err => {
