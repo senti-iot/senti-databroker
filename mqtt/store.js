32a2a243c9251962a36ea1676359d409d497ca18
Andrei <andrei.tudor27@gmail.com>
2019-05-30 17:47:05 +0200

    Fix Time

--- mqtt/store.js
+++ mqtt/store.js
@@ -29,7 +29,7 @@ class StoreMqttHandler extends MqttHandler {
 			`
 			let query = `INSERT INTO Device_data
 			(data, topic, created, device_id)
-			SELECT '${JSON.stringify(pData)}', '', '${moment.unix(pData.time).isValid() ? moment.unix(pData.time) : 'NOW()'}', Device.id as device_id from Registry
+			SELECT '${JSON.stringify(pData)}', '', ${moment.unix(pData.time).isValid() ? `'${moment.unix(pData.time)}'` : 'NOW()'}, Device.id as device_id from Registry
 			INNER JOIN Device ON Registry.id = Device.reg_id
 			INNER JOIN Customer ON Customer.id = Registry.customer_id
 			where Customer.uuid='${customerID}' AND Device.uuid='${deviceName}' AND Registry.uuid='${regName}'
