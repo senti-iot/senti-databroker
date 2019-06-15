e394f61c9495cda78f26c2fb1dd5d53a6e700248
Andrei <andrei.tudor27@gmail.com>
2019-06-15 17:19:14 +0200

    get Device Data Averages

--- api/deviceData/getDeviceData.js
+++ api/deviceData/getDeviceData.js
@@ -6,7 +6,7 @@ var mysqlConn = require('../../mysql/mysql_handler')
 const moment = require('moment')
 const engineAPI = require('../engine/engine')
 
-router.get('/:version/devicedata-clean/:deviceID/:from/:to/:type/:nId', async (req, res, next) => {
+router.get('/:version/devicedata-clean/:deviceID/:from/:to/:type/:nId/:deviceType?/:chartType?', async (req, res, next) => {
 	let apiVersion = req.params.version
 	let authToken = req.headers.auth
 	let deviceID = req.params.deviceID
@@ -14,25 +14,76 @@ router.get('/:version/devicedata-clean/:deviceID/:from/:to/:type/:nId', async (r
 	let to = req.params.to
 	let type = req.params.type
 	let nId = req.params.nId
+	let chartType = parseInt(req.params.chartType)
+	let deviceTypeID = req.params.deviceType
 	if (verifyAPIVersion(apiVersion)) {
 		if (authenticate(authToken)) {
-			let query = `SELECT id, \`data\`, created, device_id
-			FROM Device_data_clean	
-			WHERE device_id=? AND \`data\` NOT LIKE '%null%' AND created >= ? AND created <= ? ORDER BY created`
-			console.log(deviceID, from ,to, nId)
+			if (deviceID === 'all' && deviceTypeID) {
+				let q1 = `SELECT (sum(c.\`data\`->>'$.${type}')/count(c.\`data\`)) as avrg from Device_data_clean c
+				Inner join Device d on d.id = c.device_id
+				where d.type_id = ? and created >= ? AND created <= ? ORDER BY created and \`data\`->>'$.${type}' not like '%null%'`
+				let q2 = `SELECT created, AVG(\`data\`->>'$.${type}') as avrg from Device_data_clean
+				inner join Device d on d.id = device_id
+				where d.type_id = ? and \`data\`->>'$.${type}' not like '%null%' and created >= ? AND created <= ?
+				group by created
+				order by created asc`
+				console.log(chartType)
+				await mysqlConn.query(chartType === 0 ? q2 : q1, [deviceTypeID, from, to]).then(async rs => {
+					let data = rs[0]
+					if (chartType === 1) {
+						data = rs[0][0].avrg
+						if (nId > 0) {
+							let cData = await engineAPI.post('/', { nIds: [nId], data: data }).then((rss) => {
+								console.log('EngineAPI Status:', rss.status);
+								console.log('EngineAPI Response', rss.data)
+								return rss.ok ? rss.data : null
+							})
+							res.status(200).json(cData)
+						}
+						else {
+							res.status(200).json(data)
+						}
+					}
+					else {
+						let cleanData = {}
+						data.forEach(r => {
+							cleanData[moment(r.created).format('YYYY-MM-DD HH:mm:ss')] = r.avrg
+						})
+						if (nId > 0) {
+							let cData = await engineAPI.post('/', { nIds: [nId], data: data }).then((rss) => {
+								console.log('EngineAPI Status:', rss.status);
+								console.log('EngineAPI Response', rss.data)
+								return rss.ok ? rss.data : null
+							})
+							res.status(200).json(cData)
+						}
+						else {
+							res.status(200).json(cleanData)
+						}
+					}
+				}).catch(err => { 
+					console.log(err)
+				})
+			}
+			else { 
+
+				let query = `SELECT id, \`data\`, created, device_id
+				FROM Device_data_clean	
+				WHERE device_id=? AND \`data\` NOT LIKE '%null%' AND created >= ? AND created <= ? ORDER BY created`
+				console.log(deviceID, from ,to, nId)
 			await mysqlConn.query(query, [deviceID, from, to]).then(async rs => {
 				let rawData = rs[0]
 				let cleanData = {}
 				rawData.forEach(r => {
-					cleanData[moment(r.created).utc().format('YYYY-MM-DD HH:mm:ss')] = r.data[type]
+					cleanData[moment(r.created).format('YYYY-MM-DD HH:mm:ss')] = r.data[type]
 				})
 				console.log(rawData)
 				console.log(cleanData)
 				if(nId>0) {
-					let cData = await engineAPI.post('/', {nIds: [nId], data: cleanData}).then(rs => { 
-						console.log('EngineAPI Status:', rs.status); 
-						console.log('EngineAPI Response', rs.data)
-						return rs.ok ? rs.data : null 
+					let cData = await engineAPI.post('/', {nIds: [nId], data: cleanData}).then(rss => { 
+						console.log('EngineAPI Status:', rss.status); 
+						console.log('EngineAPI Response', rss.data)
+						return rss.ok ? rss.data : null 
 					})
 					return res.status(200).json(cData)
 				}
@@ -40,6 +91,7 @@ router.get('/:version/devicedata-clean/:deviceID/:from/:to/:type/:nId', async (r
 			}).catch(err => {
 				if (err) { res.status(500).json({ err, query }) }
 			})
+		}
 		} else {
 			res.status(403).json('Unauthorized Access! 403')
 			console.log('Unauthorized Access!')
