c04d5102cc6cfd90cc507e439bd57f23b96a3fd2
Andrei Tudor <andrei.tudor27@gmail.com>
2020-01-20 16:58:09 +0100

    Deploy

--- scripts/service-restart.sh
+++ scripts/service-restart.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # chmod 700 api-restart.sh
 
-if [[ "$1" == "master" ]]; then 
+if [[ "$1" == "master" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/databroker/production
 	systemctl restart senti-databroker.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Data Broker MASTER updated and restarted!"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Data Broker MASTER updated and restarted!"}' $2
 	echo
 	exit 0
 fi
 
-if [[ "$1" == "dev" ]]; then 
+if [[ "$1" == "dev" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/databroker/development
 	systemctl restart senti-databroker-dev.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Data Broker DEV updated and restarted!"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Data Broker DEV updated and restarted!"}' $2
 	echo
 	exit 0
 fi
