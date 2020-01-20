f96fedd08481d45a0f372d27cdef1680012b82e7
Andrei Tudor <andrei.tudor27@gmail.com>
2020-01-20 16:31:50 +0100

    try again

--- scripts/service-deploy.sh
+++ scripts/service-deploy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-if [[ "$1" == "master" ]]; then 
+if [[ "$1" == "master" ]]; then
 	echo
-	echo Deploying Senti Data Broker $1 ... 
+	echo Deploying Senti Data Broker $1 ...
 	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/databroker/production
 	echo
-	echo Restarting Senti Data Broker service: $1 ... 
+	echo Restarting Senti Data Broker service: $1 ...
 	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/services/databroker/production/scripts/service-restart.sh master'
 	echo
 	echo Deployment to Senti Data Broker $1 and restart done!
 	exit 0
-fi 
+fi
 
-if [[ "$1" == "dev" ]]; then 
+if [[ "$1" == "dev" ]]; then
 	echo
-	echo Deploying Senti Data Broker $1 ... 
+	echo Deploying Senti Data Broker $1 ...
 	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/databroker/development
 	echo
-	echo Restarting Senti Data Broker service: $1 ... 
-	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/services/databroker/development/scripts/service-restart.sh dev'
+	echo Restarting Senti Data Broker service: $1 ...
+	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/services/databroker/development/scripts/service-restart.sh dev $2'
 	echo
 	echo Deployment to Senti Data Broker $1 and restart done!
 	exit 0
