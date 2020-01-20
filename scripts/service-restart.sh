57832d457de2fcc8331f072e31ce42006c5eca9b
Andrei Tudor <andrei.tudor27@gmail.com>
2020-01-20 16:27:39 +0100

    Slack Hook

--- scripts/service-restart.sh
+++ scripts/service-restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # chmod 700 api-restart.sh
 
-if [[ "$1" == "master" ]]; then 
+if [[ "$1" == "master" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/databroker/production
 	systemctl restart senti-databroker.service
 	# Senti Slack Workspace
@@ -10,11 +10,11 @@ if [[ "$1" == "master" ]]; then
 	exit 0
 fi
 
-if [[ "$1" == "dev" ]]; then 
+if [[ "$1" == "dev" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/databroker/development
 	systemctl restart senti-databroker-dev.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Data Broker DEV updated and restarted!"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Data Broker DEV updated and restarted!"}' $SLACK_HOOK
 	echo
 	exit 0
 fi
