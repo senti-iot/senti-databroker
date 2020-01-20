55feffdea5a0f24ec8028b2c0d3a296c34c78f5b
Andrei Tudor <andrei.tudor27@gmail.com>
2020-01-20 16:50:13 +0100

    On Master too

--- scripts/service-restart.sh
+++ scripts/service-restart.sh
@@ -5,7 +5,7 @@ if [[ "$1" == "master" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/databroker/production
 	systemctl restart senti-databroker.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Data Broker MASTER updated and restarted!"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Data Broker MASTER updated and restarted!"}' $2
 	echo
 	exit 0
 fi
