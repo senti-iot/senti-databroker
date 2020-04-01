a53f13603396736049f9e12019f915a278d2bc82
Mikkel Broberg <mb@webhouse.dk>
2020-04-01 14:15:04 +0200

    qualitative/byhour added

--- api/v2/climaidInsight/climaidInsight.js
+++ api/v2/climaidInsight/climaidInsight.js
@@ -40,6 +40,48 @@ const colorState = (d, cfg) => {
 	return { ts: d.ts, color: Math.max(T_color, RH_color, CO2_color) }
 }
 
+router.post('/v2/climaidinsight/qualitative/byhour/:from/:to', async (req, res) => {
+	let lease = await authClient.getLease(req)
+	if (lease === false) {
+		res.status(401).json()
+		return
+	}
+	if (!(req.body && req.body.devices && req.body.config)) {
+		res.status(400).json()
+		return
+	}
+	let queryUUIDs = (req.body.devices.length) ? req.body.devices : []
+	if (queryUUIDs.length === 0) {
+		res.status(404).json([])
+		return
+	}
+	let clause = (queryUUIDs.length > 0) ? ' AND ddc.device_id IN (?' + ",?".repeat(queryUUIDs.length - 1) + ') ' : ''
+	let select = `SELECT 
+					CONCAT(date(created), ' ', hour(created)) AS ts, 
+					sum(data->'$.cold') as cold,
+					sum(data->'$.warm') as warm,
+					sum(data->'$.noisy') as noisy,
+					sum(data->'$.tired') as tired,
+					sum(data->'$.windy') as windy,
+					sum(data->'$.blinded') as blinded,
+					sum(data->'$.heavyair') as heavyair,
+					sum(data->'$.lighting') as lighting,
+					sum(data->'$.itchyeyes') as itchyeyes,
+					sum(data->'$.concentration') as concentration
+				FROM deviceDataClean ddc
+				WHERE ddc.created >= ? 
+					AND ddc.created < ?
+					${clause}
+				GROUP BY DATE(created), HOUR(created)`
+	console.log(mysqlConn.format(select, [req.params.from, req.params.to, ...queryUUIDs]))
+	let rs = await mysqlConn.query(select, [req.params.from, req.params.to, ...queryUUIDs])
+	if (rs[0].length === 0) {
+		res.status(404).json([])
+		return
+	}
+	res.status(200).json(rs[0])
+})
+
 router.post('/v2/climaidinsight/colorstate/room', async (req, res) => {
 	let lease = await authClient.getLease(req)
 	if (lease === false) {
@@ -57,24 +99,24 @@ router.post('/v2/climaidinsight/colorstate/room', async (req, res) => {
 	}
 	let clause = (queryUUIDs.length > 0) ? ' AND ddc.device_id IN (?' + ",?".repeat(queryUUIDs.length - 1) + ') ' : ''
 	let select = `SELECT 
-							CONCAT(date(created), ' ', hour(created)) AS ts, 
-							SUM(t)/count(*) AS T_gen60,
-							SUM(h)/count(*) AS RH_gen60,
-							SUM(co2)/count(*) AS CO2_gen60,
-							SUM(t)/count(*) AS T_rel,
-							SUM(h)/count(*) AS RH_rel,
-							SUM(co2)/count(*) AS CO2_rel
+						CONCAT(date(created), ' ', hour(created)) AS ts, 
+						SUM(t)/count(*) AS T_gen60,
+						SUM(h)/count(*) AS RH_gen60,
+						SUM(co2)/count(*) AS CO2_gen60,
+						SUM(t)/count(*) AS T_rel,
+						SUM(h)/count(*) AS RH_rel,
+						SUM(co2)/count(*) AS CO2_rel
+					FROM (
+						SELECT data->'$.temperature' AS t, data->'$.humidity' AS h,  data->'$.co2' AS co2, d.device_id, d.created
 						FROM (
-							SELECT data->'$.temperature' AS t, data->'$.humidity' AS h,  data->'$.co2' AS co2, d.device_id, d.created
-							FROM (
-								SELECT ddc.device_id, MAX(ddc.created) AS created
-								FROM deviceDataClean ddc
-								WHERE 1
-									${clause}
-								GROUP BY ddc.device_id
-							) t
-							INNER JOIN deviceDataClean d ON t.device_id=d.device_id AND t.created=d.created
-						) t2`
+							SELECT ddc.device_id, MAX(ddc.created) AS created
+							FROM deviceDataClean ddc
+							WHERE 1
+								${clause}
+							GROUP BY ddc.device_id
+						) t
+						INNER JOIN deviceDataClean d ON t.device_id=d.device_id AND t.created=d.created
+					) t2`
 	console.log(mysqlConn.format(select, [...queryUUIDs]))
 	let rs = await mysqlConn.query(select, [...queryUUIDs])
 	if (rs[0].length === 0) {
