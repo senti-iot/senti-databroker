4b8547f3eba1b7b43a940c4283d5c8354a9ea250
Mikkel Broberg <mb@webhouse.dk>
2020-04-16 11:45:04 +0200

    totalusagebyday on org

--- api/v2/waterworks/waterworks.js
+++ api/v2/waterworks/waterworks.js
@@ -285,6 +285,80 @@ router.post('/v2/waterworks/data/usagebyday/:from/:to', async (req, res) => {
 	let rs = await mysqlConn.query(select, [req.params.from, req.params.to, ...queryUUIDs, req.params.from, req.params.to, ...queryUUIDs])
 	res.status(200).json(rs[0])
 })
+router.get('/v2/waterworks/data/totalusagebyday/:orguuid/:from/:to', async (req, res) => {
+	let lease = await authClient.getLease(req)
+	if (lease === false) {
+		res.status(401).json()
+		return
+	}
+
+	let resources = await aclClient.findResources(lease.uuid, req.params.orguuid, sentiAclResourceType.device, sentiAclPriviledge.device.read)
+	if (resources.length === 0) {
+		res.status(404).json([])
+		return
+	}
+	let queryUUIDs = (resources.length > 0) ? resources.map(item => { return item.uuid }) : []
+	if (queryUUIDs.length === 0) {
+		res.status(404).json([])
+		return
+	}
+	// let access = await aclClient.testResources(lease.uuid, queryUUIDs, [sentiAclPriviledge.device.read])
+	// if (access.allowed === false) {
+	// 	res.status(403).json()
+	// 	return
+	// }
+	let clause = (queryUUIDs.length > 0) ? ' AND d.uuid IN (?' + ",?".repeat(queryUUIDs.length - 1) + ') ' : ''
+	let select = `SELECT sum(vdiff/diff) as totalFlowPerSecond, sum((vdiff/diff)*86400) as totalFlowPerDay, date(t) AS d
+					FROM (
+						SELECT d4.val-d5.val as vdiff, time_to_sec((timediff(d4.t,d5.t))) as diff, d4.t, d4.did
+						FROM (
+							SELECT val, t, @row:=@row+1 as r, d3.did
+							FROM ( SELECT @row:=0) foo
+							INNER JOIN (
+								SELECT MAX(ddd.val) AS val, max(ddd.t) AS t, ddd.did
+								FROM (
+									SELECT dd.val, YEAR(dd.t) as y, MONTH(dd.t) as m, DAY(dd.t) AS d, dd.t, dd.did
+									FROM (
+										SELECT dd.created AS t, dd.data->'$.volume' as val, dd.device_id AS did
+										FROM device d 
+											INNER JOIN deviceDataClean dd 
+												ON dd.device_id = d.id 
+													AND dd.created >= ?
+													AND dd.created <= ?
+										WHERE 1 ${clause}
+									) dd
+									WHERE NOT ISNULL(val)
+								) ddd
+								GROUP BY did,y,m,d
+							) d3 ON 1
+						) d4
+						INNER JOIN (
+							SELECT val, t, @row2:=@row2+1 as r, did
+							FROM ( SELECT @row2:=0) foo
+							INNER JOIN (
+								SELECT MAX(ddd.val) AS val, max(ddd.t) AS t, ddd.did
+								FROM (
+									SELECT dd.val, YEAR(dd.t) as y, MONTH(dd.t) as m, DAY(dd.t) AS d, dd.t, dd.did
+									FROM (
+										SELECT dd.created AS t, dd.data->'$.volume' as val, dd.device_id AS did
+										FROM device d 
+											INNER JOIN deviceDataClean dd 
+												ON dd.device_id = d.id 
+													AND dd.created >= ?
+													AND dd.created <= ?
+										WHERE 1 ${clause}
+									) dd
+									WHERE NOT ISNULL(val)
+								) ddd
+								GROUP BY did,y,m,d
+							) ddd ON 1
+						) d5 ON d5.r=d4.r-1 AND d4.did=d5.did
+					) kiddingme
+					GROUP BY d;`
+	let rs = await mysqlConn.query(select, [req.params.from, req.params.to, ...queryUUIDs, req.params.from, req.params.to, ...queryUUIDs])
+	res.status(200).json(rs[0])
+})
+
 router.post('/v2/waterworks/data/totalusagebyday/:from/:to', async (req, res) => {
 	let lease = await authClient.getLease(req)
 	if (lease === false) {
