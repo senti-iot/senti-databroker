5f3f6fa69eb54a449e22d36c575446bbd4b6d9fe
Andrei Tudor <andrei.tudor27@gmail.com>
2020-05-28 17:25:01 +0200

    include it in the service

--- server.js
+++ server.js
@@ -65,6 +65,7 @@ const deleteReg = require('./api/registry/deleteRegistry')
 // V2
 const createRegistryV2 = require('./api/v2/registry/createRegistry')
 const getRegistryV2 = require('./api/v2/registry/getRegistry')
+const getRegistriesV2 = require('./api/v2/registry/getRegistries')
 
 //#endregion
 
@@ -114,7 +115,7 @@ app.use([getDeviceV2, getDevicesV2, createDeviceV2])
 /**
  * Registries
  */
-app.use([createRegistryV2, getRegistryV2])
+app.use([createRegistryV2, getRegistryV2, getRegistriesV2])
 app.use([getWaterworksV2])
 app.use([climaidInsightV2])
 
