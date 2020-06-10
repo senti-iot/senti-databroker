baafcd8d44613f6f3c415f13b129bc51ac08f150
Andrei Tudor <andrei.tudor27@gmail.com>
2020-06-10 13:52:11 +0200

    Get registries under org

--- api/v2/registry/getRegistries.js
+++ api/v2/registry/getRegistries.js
@@ -40,4 +40,33 @@ router.get('/v2/registries', async (req, res) => {
 	res.status(200).json(registries)
 })
 
+router.get('/v2/registries/:orguuid', async (req, res) => {
+	/**
+	 * Check if lease is valid
+	 */
+	let lease = await authClient.getLease(req)
+	if (lease === false) {
+		res.status(401).json()
+		return
+	}
+	/**
+	 * Return all resources the user has access
+	 */
+	let resources = await aclClient.findResources(lease.uuid, req.params.orguuid, sentiAclResourceType.registry, sentiAclPriviledge.registry.read)
+	if (resources.length === 0) {
+		res.status(404).json([])
+		return
+	}
+	/**
+	 * get the list of UUIDs for the registries
+	 */
+	let queryUUIDs = resources.map(i => i.uuid)
+	/**
+	 * Generate the Registries body
+	 */
+	let registries = await registryService.getRegistriesByUUID(queryUUIDs)
+	// console.log(registries)
+	res.status(200).json(registries)
+})
+
 module.exports = router
