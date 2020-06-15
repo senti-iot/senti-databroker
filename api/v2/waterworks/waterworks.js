6808724126634f8c528438866da0fefb3026ffac
Mikkel Broberg <mb@webhouse.dk>
2020-06-15 15:18:22 +0200

    waterworks byhour first tests

--- api/v2/waterworks/waterworks.js
+++ api/v2/waterworks/waterworks.js
@@ -306,6 +306,70 @@ router.post('/v2/waterworks/data/usagebyday/:from/:to', async (req, res) => {
 	let rs = await mysqlConn.query(select, [req.params.from, req.params.to, ...queryUUIDs, req.params.from, req.params.to, ...queryUUIDs])
 	res.status(200).json(rs[0])
 })
+router.get('/v2/waterworks/data/usagebyhour/:from/:to', async (req, res) => {
+	let lease = await authClient.getLease(req)
+	if (lease === false) {
+		res.status(401).json()
+		return
+	}
+	let resources = await aclClient.findResources(lease.uuid, '00000000-0000-0000-0000-000000000000', sentiAclResourceType.device, sentiAclPriviledge.device.read)
+	if (resources.length === 0) {
+		res.status(404).json([])
+		return
+	}
+	let queryUUIDs = (resources.length > 0) ? resources.map(item => { return item.uuid }) : []
+	let clause = (queryUUIDs.length > 0) ? ' AND d.uuid IN (?' + ",?".repeat(queryUUIDs.length - 1) + ') ' : ''
+	let select = `SELECT vdiff/diff as averageFlowPerSecond, (vdiff/diff)*3600 as averageFlowPerHour, CONCAT(DATE(t),' ',HOUR(t),':00:00') AS datehour, did
+					FROM (
+						SELECT d4.val-d5.val as vdiff, time_to_sec((timediff(d4.t,d5.t))) as diff, d4.t, d4.did
+						FROM (
+							SELECT val, t, @row:=@row+1 as r, d3.did
+							FROM ( SELECT @row:=0) foo
+							INNER JOIN (
+								SELECT MAX(ddd.val) AS val, max(ddd.t) AS t, ddd.did
+								FROM (
+									SELECT dd.val, DATE(dd.t) AS d, HOUR(dd.t) As h, dd.t, dd.did
+									FROM (
+										SELECT dd.created AS t, dd.data->'$.volume' as val, dd.device_id AS did
+										FROM device d 
+											INNER JOIN deviceDataClean dd 
+												ON dd.device_id = d.id 
+													AND dd.created >= DATE_SUB(?, INTERVAL 1 HOUR)
+													AND dd.created < ?
+										WHERE 1 ${clause}
+									) dd
+									WHERE NOT ISNULL(val)
+								) ddd
+								GROUP BY did,d,h
+							) d3 ON 1
+						) d4
+						INNER JOIN (
+							SELECT val, t, @row2:=@row2+1 as r, did
+							FROM ( SELECT @row2:=0) foo
+							INNER JOIN (
+								SELECT MAX(ddd.val) AS val, max(ddd.t) AS t, ddd.did
+								FROM (
+									SELECT dd.val, DATE(dd.t) AS d, HOUR(dd.t) As h, dd.t, dd.did
+									FROM (
+										SELECT dd.created AS t, dd.data->'$.volume' as val, dd.device_id AS did
+										FROM device d 
+											INNER JOIN deviceDataClean dd 
+												ON dd.device_id = d.id 
+													AND dd.created >= DATE_SUB(?, INTERVAL 1 HOUR)
+													AND dd.created < ?
+										WHERE 1 ${clause}
+									) dd
+									WHERE NOT ISNULL(val)
+								) ddd
+								GROUP BY did,d,h
+							) ddd ON 1
+						) d5 ON d5.r=d4.r-1 AND d4.did=d5.did
+					) kiddingme;`
+	console.log(mysqlConn.format(select, [req.params.from, req.params.to, ...queryUUIDs, req.params.from, req.params.to, ...queryUUIDs]))
+	let rs = await mysqlConn.query(select, [req.params.from, req.params.to, ...queryUUIDs, req.params.from, req.params.to, ...queryUUIDs])
+	res.status(200).json(rs[0])
+})
+
 router.get('/v2/waterworks/data/totalusagebyday/:orguuid/:from/:to', async (req, res) => {
 	let lease = await authClient.getLease(req)
 	if (lease === false) {
@@ -743,6 +807,72 @@ router.get('/v2/waterworks/data/benchmark/:orguuid/:from/:to', async (req, res) 
 	let rs = await mysqlConn.query(select, [req.params.orguuid, req.params.from, req.params.to, req.params.orguuid, req.params.from, req.params.to])
 	res.status(200).json(rs[0])
 })
+
+router.get('/v2/waterworks/data/benchmarkbyhour/:orguuid/:from/:to', async (req, res) => {
+	let lease = await authClient.getLease(req)
+	if (lease === false) {
+		res.status(401).json()
+		return
+	}
+	let select = `SELECT	3600*totalflow as totalFlowPerHour
+						, totalflow/hourcount as averageFlowPerSecond
+						, 3600*totalflow/hourcount as averageFlowPerHour
+						, d AS datehour
+					FROM ( 
+					SELECT SUM(flow) AS totalflow, count(*) AS hourcount, CONCAT(date(t),' ',HOUR(t),':00:00') AS d
+					FROM ( 
+						SELECT vdiff/diff as flow, t, did 
+						FROM ( 
+							SELECT d4.val-d5.val as vdiff, time_to_sec((timediff(d4.t,d5.t))) as diff, d4.t, d4.did 
+							FROM ( 
+								SELECT val, t, @row:=@row+1 as r, d3.did 
+								FROM ( SELECT @row:=0) foo 
+								INNER JOIN ( 
+									SELECT MAX(ddd.val) AS val, max(ddd.t) AS t, ddd.did 
+									FROM ( 
+										SELECT dd.val, DATE(dd.t) as d, HOUR(dd.t) AS h, dd.t, dd.did 
+										FROM ( 
+											SELECT dd.created AS t, dd.data->'$.volume' as val, dd.device_id AS did 
+											FROM organisation o 
+												INNER JOIN registry r on o.id = r.orgId 
+												INNER JOIN device d on r.id = d.reg_id 
+												INNER JOIN deviceDataClean dd ON dd.device_id = d.id 
+											WHERE o.uuid = ? 
+												AND dd.created >= DATE_SUB(?, INTERVAL 1 HOUR) 
+												AND dd.created < ?
+										) dd 
+									) ddd 
+									GROUP BY did, d,h 
+								) d3 ON 1 
+							) d4 
+						INNER JOIN ( 
+							SELECT val, t, @row2:=@row2+1 as r, did 
+							FROM ( SELECT @row2:=0) foo 
+							INNER JOIN ( 
+								SELECT MAX(ddd.val) AS val, max(ddd.t) AS t, ddd.did 
+								FROM ( 
+									SELECT dd.val, DATE(dd.t) as d, HOUR(dd.t) AS h, dd.t, dd.did 
+									FROM ( 
+										SELECT dd.created AS t, dd.data->'$.volume' as val, dd.device_id AS did 
+										FROM organisation o 
+											INNER JOIN registry r on o.id = r.orgId 
+											INNER JOIN device d on r.id = d.reg_id 
+											INNER JOIN deviceDataClean dd ON dd.device_id = d.id 
+										WHERE o.uuid = ? 
+											AND dd.created >= DATE_SUB(?,INTERVAL 1 HOUR) 
+											AND dd.created < ?
+									) dd 
+								) ddd 
+								GROUP BY did,d,h
+							) ddd ON 1 
+						) d5 ON d5.r=d4.r-1 AND d4.did=d5.did 
+					) kiddingme ) km2 
+					GROUP BY date(t), HOUR(t)
+					) t;`
+	let rs = await mysqlConn.query(select, [req.params.orguuid, req.params.from, req.params.to, req.params.orguuid, req.params.from, req.params.to])
+	res.status(200).json(rs[0])
+})
+
 router.post('/v2/waterworks/data/benchmark/:from/:to', async (req, res) => {
 	let lease = await authClient.getLease(req)
 	if (lease === false) {
