ebc0a6a34f91cd0774225c72cbb696e4ee78068f
Mikkel Broberg <mb@webhouse.dk>
2020-06-30 15:22:16 +0200

    deviceType backend update

--- api/v2/devicetype/deleteDeviceType.js
+++ api/v2/devicetype/deleteDeviceType.js
@@ -1,49 +1,34 @@
 const express = require('express')
 const router = express.Router()
-// const { aclClient, authClient } = require('../../../server')
-// const { sentiAclPriviledge } = require('senti-apicore')
+const { aclClient, authClient } = require('../../../server')
+const { sentiAclPriviledge } = require('senti-apicore')
 
-// var mysqlConn = require('../../../mysql/mysql_handler')
+var mysqlConn = require('../../../mysql/mysql_handler')
 
-// const sentiRegistryService = require('../../../lib/registry/sentiRegistryService')
-// const registryService = new sentiRegistryService(mysqlConn)
-// const sentiDatabrokerCoreService = require('../../../lib/databrokerCore/sentiDatabrokerCoreService')
-// const sentiDataCore = new sentiDatabrokerCoreService(mysqlConn)
+const sentiDeviceTypeService = require('../../../lib/deviceType/sentiDeviceTypeService')
+const deviceTypeService = new sentiDeviceTypeService(mysqlConn)
 
 router.delete('/v2/devicetype/:uuid', async (req, res) => {
-	return res.status(404)
-	// let lease = await authClient.getLease(req)
-	// if (lease === false) {
-	// 	return res.status(401).json()
-	// }
-
-	// /**
-	//  * Check if the user has access to the registry
-	//  */
-
-	// let access = await aclClient.testPrivileges(lease.uuid, req.params.uuid, [sentiAclPriviledge.registry.delete, sentiAclPriviledge.organisation.modify])
-	// if (access.allowed === false) {
-	// 	return res.status(403).json()
-	// }
-	// /**
-	//  * Get the Registry from the DB
-	//  */
-	// let registry = await registryService.getDbRegistryByUUID(req.params.uuid)
-	// console.log(registry)
-	// if (!registry) {
-	// 	return res.status(404).json()
-	// }
-
-	// console.log('Delete registry')
-
-	// let result = await registryService.deleteRegistry(registry)
-	// await aclClient.deleteResource(registry.uuid)
-	// // let result = false
-	// console.log('[Registry] result', result)
-	// if (result) {
-	// 	return res.status(200).json(result)
-	// }
-
+	try {
+		let lease = await authClient.getLease(req)
+		if (lease === false) {
+			res.status(401).json()
+			return
+		}
+		let access = await aclClient.testPrivileges(lease.uuid, req.params.uuid, [sentiAclPriviledge.deviceType.delete])
+		if (access.allowed === false) {
+			res.status(403).json()
+			return
+		}
+		let deviceType = await deviceTypeService.getDeviceTypeByUUID(req.params.uuid)
+		if (!deviceType) {
+			return res.status(404).json()
+		}
+		res.status(200).json(await deviceTypeService.deleteDevice(deviceType))
+	}
+	catch (error) {
+		res.status(500).json({ message: error.message, stack: error.stack })
+	}
 })
 
 module.exports = router
--- api/v2/devicetype/updateDeviceType.js
+++ api/v2/devicetype/updateDeviceType.js
@@ -1,73 +1,87 @@
 const express = require('express')
 const router = express.Router()
 
-// var mysqlConn = require('../../../mysql/mysql_handler')
+var mysqlConn = require('../../../mysql/mysql_handler')
 
-// const { sentiAclPriviledge, sentiAclResourceType } = require('senti-apicore')
-// const { aclClient, authClient } = require('../../../server')
+const { sentiAclPriviledge, sentiAclResourceType } = require('senti-apicore')
+const { aclClient, authClient } = require('../../../server')
 
-// const RequestDeviceType = require('../../../lib/devicetype/dataClasses/RequestDevicetype')
+const RequestDeviceType = require('../../../lib/devicetype/dataClasses/RequestDevicetype')
 
-// const sentiDeviceTypeService = require('../../../lib/deviceType/sentiDeviceTypeService')
-// const deviceTypeService = new sentiDeviceTypeService(mysqlConn)
-// const sentiDatabrokerCoreService = require('../../../lib/databrokerCore/sentiDatabrokerCoreService')
-// const databrokerCoreService = new sentiDatabrokerCoreService(mysqlConn)
+const sentiDeviceTypeService = require('../../../lib/deviceType/sentiDeviceTypeService')
+const deviceTypeService = new sentiDeviceTypeService(mysqlConn)
+const sentiDatabrokerCoreService = require('../../../lib/databrokerCore/sentiDatabrokerCoreService')
+const databrokerCoreService = new sentiDatabrokerCoreService(mysqlConn)
 
-router.put('/v2/devicetype', async (req, res) => {
-	return res.status(404)
-	// try {
-	// 	/**
-	// 	 * Check if the user is logged in and his lease is still good
-	// 	 */
-	// 	let lease = await authClient.getLease(req)
-	// 	if (lease === false) {
-	// 		res.status(401).json()
-	// 		return
-	// 	}
-	// 	/**
-	// 	 * Create the DeviceType obj from the body request
-	// 	 */
-	// 	let requestDeviceType = new RequestDeviceType(req.body)
-	// 	/**
-	// 	 * Check if the user has access to create devicetypes in the requested Organisation
-	// 	 */
-	// 	let access = await aclClient.testPrivileges(lease.uuid, requestDeviceType.org.uuid, [sentiAclPriviledge.deviceType.create])
-	// 	if (access.allowed === false) {
-	// 		res.status(403).json()
-	// 		return
-	// 	}
-	// 	/**
-	// 	 * Add organisation id to the DeviceType obj
-	// 	 */
-	// 	requestDeviceType.orgId = await databrokerCoreService.getOrganisationIdByUUID(requestDeviceType.org.uuid)
-	// 	/**
-	// 	 * Create the DeviceType
-	// 	 */
-	// 	let deviceType = await deviceTypeService.createDeviceType(requestDeviceType)
-	// 	if (deviceType) {
-	// 		let aclOrgResources = await databrokerCoreService.getAclOrgResourcesOnName(requestDeviceType.orgId)
-	// 		/**
-	// 		 * Register the new DeviceType with the ACL
-	// 		 */
-	// 		await aclClient.registerResource(deviceType.uuid, sentiAclResourceType.deviceType)
-	// 		/**
-	// 		 * Tie the DeviceType to its organisation
-	// 		 */
-	// 		await aclClient.addResourceToParent(deviceType.uuid, aclOrgResources['devices'].uuid)
-	// 		/**
-	// 		 * Return the new DeviceType
-	// 		 */
-	// 		res.status(200).json(await deviceTypeService.getDeviceTypeById(deviceType.id))
-	// 	} else {
-	// 		/**
-	// 		 * If there is no DeviceType created, an error occured, throw 500
-	// 		 */
-	// 		return res.status(500).json(deviceType)
-	// 	}
-	// }
-	// catch (error) {
-	// 	res.status(500).json({ message: error.message, stack: error.stack })
-	// }
+router.put('/v2/devicetype/:uuid', async (req, res) => {
+	// return res.status(404)
+	try {
+		/**
+		 * Check if the user is logged in and his lease is still good
+		 */
+		let lease = await authClient.getLease(req)
+		if (lease === false) {
+			res.status(401).json()
+			return
+		}
+		/**
+		 * Check if the user has access to modify the devicetype
+		 */
+		let access = await aclClient.testPrivileges(lease.uuid, req.params.uuid, [sentiAclPriviledge.deviceType.modify])
+		if (access.allowed === false) {
+			res.status(403).json()
+			return
+		}
+		/**
+		 * Create the DeviceType obj from the body request and test against param
+		 */
+		let requestDeviceType = new RequestDeviceType(req.body)
+		if (requestDeviceType.uuid !== req.params.uuid) {
+			res.status(400).json()
+			return
+		}
+		/**
+		 * Get the existing DeviceType
+		 */
+		let deviceType = await deviceTypeService.getDeviceTypeByUUID(requestDeviceType.uuid)
+		if (!deviceType) {
+			return res.status(404).json()
+		}
+		/**
+		 * Check if the organisation has changed
+		 */
+		if (deviceType.organisation.uuid !== requestDeviceType.org.uuid) {
+			/**
+			 * Update ACL
+			 */
+			let newOrg = await sentiDataCore.getDbOrganisationByUUID(requestDeviceType.org.uuid)
+			let oldOrg = await sentiDataCore.getDbOrganisationByUUID(deviceType.organisation.uuid)
+			/**
+			 * Get the ACL Org resources
+			 */
+			let newOrgAclResources = await sentiDataCore.getAclOrgResourcesOnName(newOrg.id)
+			let oldOrgAclResources = await sentiDataCore.getAclOrgResourcesOnName(oldOrg.id)
+			/**
+			 * Update ACL with new org resources
+			 */
+			await aclClient.removeResourceFromParent(deviceType.uuid, oldOrgAclResources['devices'].uuid)
+			await aclClient.addResourceToParent(deviceType.uuid, newOrgAclResources['devices'].uuid)
+			requestDeviceType.orgId = newOrg.id
+		}
+		deviceType.assignDiff(requestDeviceType)
+		/**
+		 * Update deviceType
+		 */
+		let result = await deviceTypeService.updateDeviceType(deviceType)
+		if (result) {
+			return res.status(200).json(result)
+		} else {
+			return res.status(500).json(deviceType)
+		}
+	}
+	catch (error) {
+		res.status(500).json({ message: error.message, stack: error.stack })
+	}
 
 })
 
