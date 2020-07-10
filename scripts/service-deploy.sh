73ff2d2a55d6a25277e4d5491fb50d6ac04bb575
Andrei <at@senti.dk>
2020-07-10 16:38:43 +0200

    Merge

--- scripts/service-deploy.sh
+++ scripts/service-deploy.sh
@@ -24,3 +24,16 @@ if [[ "$1" == "dev" ]]; then
 	echo Deployment to Senti Data Broker $1 and restart done!
 	exit 0
 fi
+
+if [[ "$1" == "merge" ]]; then
+	echo
+	echo Deploying Senti Data Broker $1 ...
+	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/services/databroker/merge
+	echo
+	echo Restarting Senti Data Broker service: $1 ...
+	echo $3
+	ssh deploy@rey.webhouse.net "sudo /srv/nodejs/senti/services/databroker/merge/scripts/service-restart.sh merge $3"
+	echo
+	echo Deployment to Senti Data Broker $1 and restart done!
+	exit 0
+fi
